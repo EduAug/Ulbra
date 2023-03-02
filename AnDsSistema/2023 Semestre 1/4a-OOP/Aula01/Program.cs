8ccfb38b99e40c03bb8ef887d403b067531426e3
EduAug <edu_03@outlook.com.br>
2023-03-01 23:02:56 -0300

    Comeco Semestre 1 2023

---
+++ AnDsSistema/2023 Semestre 1/4a-OOP/Aula01/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string hellow = "Hello, World!";
+Console.WriteLine(hellow);
+Console.WriteLine("Tive de mudar o Path nas variaveis, mas deu certo");

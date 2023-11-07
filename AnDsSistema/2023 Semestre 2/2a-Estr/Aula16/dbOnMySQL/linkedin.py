5b8375433aaf414c0e4fdc1683df03df03930465
EduAug <edu_03@outlook.com.br>
2023-11-06 22:29:03 -0300

    readding, the previous was just to highlight for posterior reference

--- AnDsSistema/2023 Semestre 2/2a-Estr/Aula16/dbOnMySQL/linkedin.py
+++ AnDsSistema/2023 Semestre 2/2a-Estr/Aula16/dbOnMySQL/linkedin.py
@@ -4,7 +4,9 @@ def openConnection():
     conn = mysql.connector.connect(
     host="db4free.net",
     port=3306,
-    
+    database="linkedin123",
+    user="natanhmc",
+    password="1q2w3e4r5t"
     )
     conn.start_transaction()
 

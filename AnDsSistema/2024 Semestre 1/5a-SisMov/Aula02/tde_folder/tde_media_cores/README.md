16e1d9e62105353c505d0db50592974f9cf66945
EduAug <edu_03@outlook.com.br>
2024-03-14 21:58:58 -0300

    Modelo UseCase + Dart

---
+++ AnDsSistema/2024 Semestre 1/5a-SisMov/Aula02/tde_folder/tde_media_cores/README.md
@@ -0,0 +1,1 @@
+A sample command-line application with an entrypoint in `bin/`, TDE referente ao cálculo de média de uma lista de números, e uma "tradução" para as cores primárias.
---
+++ AnDsSistema/2024 Semestre 1/5a-SisMov/Aula02/tde_folder/tde_media_cores/bin/tde_media_cores.dart
@@ -0,0 +1,39 @@
+double getAvg(List<int> values){
+    if(values.isEmpty){
+      print("Lista vazia");
+      return 0.0;
+    }else if(values == Null){
+      print("Lista... nula?");
+      return 0.0;
+    }
+    int totalSum = 0;
+    for(int i = 0; i < values.length; i++){
+      totalSum += values[i];
+    }
+    return totalSum / values.length;
+  }
+
+String translateColor(String color){
+  switch(color.toLowerCase()){
+    case "red":
+      return "Vermelho";
+    case "blue":
+      return "Azul";
+    case "green":
+      return "Verde";
+    case "yellow":
+      return "Amarelo";
+    default:
+      return "Qualquer outra cor.";
+  }
+}
+
+void main() {
+  List<int> entryValues = [1,573,9,32,2];
+  //List<int> entryValues = [];
+  //var entryValues = null;
+  print(getAvg(entryValues));   
+  //Coincidentemente, o resultado é 123.4
+
+  print(translateColor("yelLOw"));
+}

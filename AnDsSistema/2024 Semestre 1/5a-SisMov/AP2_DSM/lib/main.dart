108395ee134093dd7a07b2f363104548625c61f8
EduAug <edu_03@outlook.com.br>
2024-06-12 09:18:40 -0300

    Widgets AP2

--- AnDsSistema/2024 Semestre 1/5a-SisMov/AP2_DSM/lib/main.dart
+++ AnDsSistema/2024 Semestre 1/5a-SisMov/AP2_DSM/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ap2_pokedex/pages/home_page.dart';
 import 'package:ap2_pokedex/services/poke_api.dart';
 import 'package:ap2_pokedex/models/pokemon.dart';
 import 'package:flutter/material.dart';
@@ -15,19 +16,20 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   PokeApi api = PokeApi();
+
+  late Future<List<Pokemon>> _pokeFuture;
   late List<Pokemon> _characters;
   late List<Pokemon> _charFilter;
 
   @override
-  void initState() {
+  initState(){
     super.initState();
-    _fetchAllChars();
+    _pokeFuture = _fetchAllChars();
   }
 
   Future<List<Pokemon>> _fetchAllChars() async {
     _characters = await api.fetchMons();
     _charFilter = _characters;
-
     return _characters;
   }
 
@@ -80,7 +82,6 @@ class _MyAppState extends State<MyApp> {
         return Colors.white;
     }
   }
-
   Color _getTextColor(Color bgColor){
     return bgColor.computeLuminance() > 0.5 ? Colors.black : Colors.white;
   }
@@ -89,88 +90,13 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("PokéDex v2",style:
-            TextStyle(color: Colors.white),),
-          backgroundColor: Colors.red,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: FutureBuilder<List<Pokemon>>(
-                  future: api.fetchMons(), builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(color: Colors.red,),
-                  );
-                } else if (snapshot.hasData) {
-                  return Expanded(
-                    child: ListView.separated(itemBuilder: (context, ind) {
-                      List<Color> colors = _charFilter[ind].typing.map(_getColorFromType).toList();
-                      Color bgColor;
-                      Color txColor;
-
-                      if (colors.length > 1){
-                        bgColor = Colors.transparent;
-                        txColor = Colors.black;
-                      } else {
-                        bgColor = colors.first;
-                        txColor = _getTextColor(bgColor);
-                      }
-                      return Container(
-                        color: colors.length > 1 ? null : bgColor,
-                        decoration: colors.length > 1 ? BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: colors,
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                          ),
-                        ):null,
-                        child: ListTile(
-                          title: Row(
-                            children: [
-                              Text('#${_charFilter[ind].regionalDexNumber} / #${_charFilter[ind].dexNumber}',
-                              style: TextStyle(color: txColor),),
-                              const SizedBox(width: 40),
-                              Text(_charFilter[ind].name,
-                              style: TextStyle(color: txColor),),
-                            ],
-                          ),
-                          subtitle: Text('${_charFilter[ind].typing}',
-                          style: TextStyle(color: txColor),),
-                          leading: Image.network(_charFilter[ind].image),
-                        ),
-                      );
-                    }, separatorBuilder: (_, __) {
-                      return const Divider();
-                    },
-                        itemCount: _charFilter.length
-                    ),
-                  );
-                } else if (snapshot.hasError){
-                  return Center(
-                    child: Text('Error: ${snapshot.error}'),
-                  );
-                }
-                return Container();
-              }),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                onChanged: (inputString) {
-                  _filterCharacters(inputString);
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Search ...",
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(
+        pokeFuture: _pokeFuture,
+        charFilter: _charFilter,
+        filterCharacter: _filterCharacters,
+        getColorFromType: _getColorFromType,
+        getTextColor: _getTextColor
+      )
     );
   }
 }
--- AnDsSistema/2024 Semestre 1/5a-SisMov/AP2_DSM/lib/services/poke_api.dart
+++ AnDsSistema/2024 Semestre 1/5a-SisMov/AP2_DSM/lib/services/poke_api.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import "package:ap2_pokedex/constants.dart";
 import 'package:ap2_pokedex/models/pokemon.dart';
 import 'package:http/http.dart' as http;
@@ -8,10 +7,9 @@ class PokeApi {
   Future<List<Pokemon>> fetchMons() async{
     final response = await http.get(Uri.parse('$BASE_URL/pokemon?offset=493&limit=156'));
     if(response.statusCode == 200){
-      var listReturn = jsonDecode(response.body)['results'] as List;
+      List listReturn = jsonDecode(response.body)['results'];
 
       List<String> dexUrls = listReturn.map((allPokemonDexReturn) => allPokemonDexReturn['url'] as String).toList();
-
       List<Future<Pokemon>> dexForEach = dexUrls.map((individualUrl) => fetchMonDetails(individualUrl)).toList();
 
       List<Pokemon> allOfThem = await Future.wait(dexForEach);
